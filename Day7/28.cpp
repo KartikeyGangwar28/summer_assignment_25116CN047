6988fa08ae2ef4815b1901f282c2030a98615bbe
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-06-07 20:37:36 +0530

    Update 28.cpp

--- Day7/28.cpp
+++ Day7/28.cpp
@@ -10,5 +10,13 @@ if(n==0){
     
 }
 int main(){
-std::cout<<reverseDigits(0,12344);
+    int n;
+    std::cout<<"ENTER THE NUMBER U WANT TO REVERSE: ";
+    std::cin>>n;
+    if(n<10&&n>-10){
+        std::cout<<"REVERSE OF SINGLE DIGIT INTEGER IS INTEGER ITSELF: ";
+        return 0;
+    }
+    int reversed=reverseDigits(0,n);
+    std::cout<<reversed;
 }

b0da29047e139001997591c50636c9211816752c
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-06-07 20:34:41 +0530

    Update 28.cpp

--- Day7/28.cpp
+++ Day7/28.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-int function(int rev,int n){
+int reverseDigits(int rev,int n){
     rev=(rev*10)+(n%10);
     n=n/10;
 if(n==0){
         return rev;
     }
-    return function(rev,n);
+    return reverseDigits(rev,n);
 
     
 }
 int main(){
-std::cout<<function(0,12344);
+std::cout<<reverseDigits(0,12344);
 }

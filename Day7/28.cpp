400ef92a185b113e0ee449dfe3593711b25ebaaf
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-06-07 20:42:01 +0530

    Fix output for single digit integer reversal
    Updated output message for single digit integers.

--- Day7/28.cpp
+++ Day7/28.cpp
@@ -14,7 +14,7 @@ int main(){
     std::cout<<"ENTER THE NUMBER U WANT TO REVERSE: ";
     std::cin>>n;
     if(n<10&&n>-10){
-        std::cout<<"REVERSE OF SINGLE DIGIT INTEGER IS INTEGER ITSELF: ";
+        std::cout<<"REVERSE OF SINGLE DIGIT INTEGER IS INTEGER ITSELF: "<<n;
         return 0;
     }
     int reversed=reverseDigits(0,n);
